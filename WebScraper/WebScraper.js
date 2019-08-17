facdd1a88bbf452173dd823c1a170e8b9475aa13
Zachary Wasserman <zwasser2@illinois.edu>
2019-08-16 19:57:05 -0700

    Basic reddit highlight and href implemented

--- WebScraper/WebScraper.js
+++ WebScraper/WebScraper.js
@@ -8,20 +8,18 @@ let scrape = async () => {
 
     let counter = 0
     let insideCounter = 1
-    await page.goto('https://knowyourmeme.com/memes/all/page/1', {waitUntil: 'domcontentloaded'});
+    await page.goto('https://knowyourmeme.com/memes/page/1', {waitUntil: 'domcontentloaded'});
     await page.waitFor(1000);
     let numberOfPages = await page.evaluate(() => {
         const entriesPerPage = 16
         let numberEntries = document.getElementsByTagName('HGroup')[0].innerText
-        // I'm not sure where, but 2 '1' are thrown in front of the 'Browsing all _______ meme entries'
         numberEntries = numberEntries.replace(/\D/g,'')
         return Math.ceil(parseInt(numberEntries) / entriesPerPage)
     })
     try {
         // Closing and opening a new session results in being banned less
-        const checksPerBrowserSession = 150
+        const checksPerBrowserSession = 14
         const timeToOpenNewPage = Math.ceil(numberOfPages / checksPerBrowserSession)
-        console.log(timeToOpenNewPage)
         while (counter <= timeToOpenNewPage) {
             // Know your meme would throw a bad page at checksPerBrowserSession pages. This is to get around this.
             insideCounter = 1
@@ -42,14 +40,14 @@ let scrape = async () => {
                 })
                 allMemes = allMemes.concat(result)
                 insideCounter ++
-                await page.goto('https://knowyourmeme.com/memes/all/page/' + (counter * checksPerBrowserSession + insideCounter), {waitUntil: 'domcontentloaded'});
+                await page.goto('https://knowyourmeme.com/memes/page/' + (counter * checksPerBrowserSession + insideCounter), {waitUntil: 'domcontentloaded'});
                 await page.waitFor(1000);
             }
             counter ++
             browser.close();
             browser = await puppeteer.launch({headless: false});
             page = await browser.newPage();
-            await page.goto('https://knowyourmeme.com/memes/all/page/' + (counter * checksPerBrowserSession + 1), {waitUntil: 'domcontentloaded'});
+            await page.goto('https://knowyourmeme.com/memes/page/' + (counter * checksPerBrowserSession + 1), {waitUntil: 'domcontentloaded'});
             await page.waitFor(1000);
         }
     } catch(err) {
